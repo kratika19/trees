6e08b4c86b2a731dc6fa38bd3333f4f3bbb23b02
Kratika Mittal <kratikarajv@gmail.com>
2022-10-06 11:30:57 +0530

    first commit

---
+++ binarytrees/levelordertraverse.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+    
+    node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node* buildTree(node* root){
+    cout<<"Enter data : "<<endl;
+    int data;
+    cin>> data;
+    
+    root = new node(data);
+    
+    if(data == -1)
+        return NULL;
+        
+    cout<<"Enter left child of "<<data<<endl;
+    root->left = buildTree(root->left);
+    
+    cout<<"Enter right child of "<<data<<endl;
+    root->right = buildTree(root->right);
+    
+    return root;
+}
+
+void levelOrderTraversal(node* root)
+{
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty())
+    {
+        node* temp = q.front();
+        q.pop();
+
+        if(temp==NULL)
+        {
+            cout<<endl;
+            if(!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left)
+                q.push(temp->left); 
+            if(temp->right)
+                q.push(temp->right);
+        }
+    }
+}
+
+int main()
+{
+    node* root = NULL;
+    root = buildTree(root);
+
+    cout<<"Level Order Traversal : "<<endl;    
+    levelOrderTraversal(root);
+    return 0;
+}
